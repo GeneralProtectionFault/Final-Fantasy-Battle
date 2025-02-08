c5bf93083455de5ba61e8c3a574fa6d58d444b81
GeneralProtectionFault <fleetingmasshysteria@gmail.com>
2025-02-08 13:46:08 -0500

    Store Previous Code in new branch/update script name

--- Scripts/Battle/BattleTurn.cs
+++ Scripts/Battle/BattleTurn.cs
@@ -1,10 +1,15 @@
 using Godot;
 using System;
 
-public class BattleQueue
+public class BattleTurn
 {
 	public Enums.BattleTurn TurnType { get; }
 	
 	// Use the character or enemy performing the action to act as an index
 	public GodotObject Initiator { get; }
+	public GodotObject Target { get; }
+
+
+	
+	
 }
